e0bb09acccbac38f1999963efc73ffa645ae4309
Dridi Boukelmoune <dridi.boukelmoune@zenika.com>
2012-09-13 09:23:31 +0200

    added a reusable runner for varnishtest

---
+++ src/main/java/com/zenika/plugins/varnishtest/VarnishtestRunner.java
@@ -0,0 +1,54 @@
+package com.zenika.plugins.varnishtest;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.ExecuteException;
+import org.apache.commons.exec.ExecuteWatchdog;
+import org.apache.commons.exec.Executor;
+import org.apache.maven.plugin.Mojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+
+class VarnishtestRunner {
+	
+	private final CommandLine commandLine;
+	
+	VarnishtestRunner(CommandLine commandLine) {
+		this.commandLine = commandLine;
+	}
+	
+	void runTestCase(Mojo mojo, File testCase, int timeout) throws MojoExecutionException, MojoFailureException {
+		
+		Executor executor = new DefaultExecutor();
+		executor.setStreamHandler( new VarnishtestStreamHandler(mojo) );
+		
+		if (timeout > 0) {
+			ExecuteWatchdog watchdog = new ExecuteWatchdog( TimeUnit.SECONDS.toMillis(timeout) );
+			executor.setWatchdog(watchdog);
+		}
+		
+		CommandLine testCaseCommandLine = new CommandLine(commandLine);
+		testCaseCommandLine.addArgument(testCase.getPath());
+		
+		try {
+			mojo.getLog().info("Executing : " + testCaseCommandLine);
+			int exitValue = executor.execute(testCaseCommandLine); // TODO configure environment
+			
+			if ( executor.isFailure(exitValue) ) {
+				throw new MojoFailureException("The test case failed : " + testCase);
+			}
+		}
+		catch (ExecuteException e) {
+			throw new MojoExecutionException("ExecuteException : " + e.getMessage(), e);
+		}
+		catch (IOException e) {
+			throw new MojoExecutionException("IOException : " + e.getMessage(), e);
+		}
+		
+	}
+}
