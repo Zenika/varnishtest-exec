da54096a4e09d2045030b5213c33a991dfe2ca5e
Dridi Boukelmoune <dridi.boukelmoune@zenika.com>
2012-09-14 09:23:33 +0200

    initial implementation of the run mojo

--- src/main/java/com/zenika/plugins/varnishtest/RunMojo.java
+++ src/main/java/com/zenika/plugins/varnishtest/RunMojo.java
@@ -1,20 +1,79 @@
 package com.zenika.plugins.varnishtest;
 
 import java.io.File;
+import java.util.List;
 
+import org.apache.commons.exec.CommandLine;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.util.DirectoryScanner;
 
 @Mojo(name = "run", defaultPhase = LifecyclePhase.INTEGRATION_TEST)
 public class RunMojo extends AbstractMojo {
+
+	private static final String[] EMPTY_STRING_ARRAY = new String[] {};
+	private static final String[] DEFAULT_INCLUDE = new String[] {"src/test/varnish/**.vtc"};
+	
+	@Component
+	private MavenProject project;
 	
 	@Parameter(defaultValue = "${project.build.directory}/varnishtest")
 	private File outputDirectory;
+    
+	@Parameter
+	private List<String> includes;
 	
-	public void execute() throws MojoExecutionException {
+    @Parameter
+    protected List<String> excludes;
+	
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		
 		outputDirectory.mkdirs();
+		
+		CommandLine commandLine = new CommandLine("varnishtest");
+		commandLine.addArgument("-Dvarnishd=varnishd", false);
+		commandLine.addArgument("-Dproject.basedir=" + getBasedir(), true);
+		
+		for (String testCase : getTestCases()) {
+			VarnishtestRunner runner = new VarnishtestRunner(commandLine);
+			runner.runTestCase(this, new File(getBasedir(), testCase), 20);
+		}
+		
+	}
+	
+	private String[] getTestCases() {
+		DirectoryScanner scanner = new DirectoryScanner();
+		scanner.setBasedir(getBasedir());
+		scanner.setIncludes(getIncludes());
+		scanner.setExcludes(getExcludes());
+		scanner.scan();
+		
+		String[] testCases = scanner.getIncludedFiles();
+		return testCases != null ? testCases : EMPTY_STRING_ARRAY;
+	}
+
+	private File getBasedir() {
+		return project.getBasedir();
+	}
+
+	private String[] getExcludes() {
+		if (excludes == null || excludes.isEmpty()) {
+			return EMPTY_STRING_ARRAY;
+		}
+		return excludes.toArray(EMPTY_STRING_ARRAY);
+	}
+
+	private String[] getIncludes() {
+		if (includes == null || includes.isEmpty()) {
+			return DEFAULT_INCLUDE;
+		}
+		return includes.toArray(EMPTY_STRING_ARRAY);
 	}
 }
