130f02a04871e9f7e4693ad2c13c48c950e699c0
Dridi Boukelmoune <dridi.boukelmoune@zenika.com>
2012-09-19 09:23:46 +0200

    added macros to the command line

--- src/main/java/com/zenika/plugins/varnishtest/RunMojo.java
+++ src/main/java/com/zenika/plugins/varnishtest/RunMojo.java
@@ -2,6 +2,7 @@ package com.zenika.plugins.varnishtest;
 
 import java.io.File;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.exec.CommandLine;
 import org.apache.maven.plugin.AbstractMojo;
@@ -14,29 +15,35 @@ import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.util.DirectoryScanner;
 
-@Mojo(name = "run", defaultPhase = LifecyclePhase.INTEGRATION_TEST)
+@Mojo(name = "run",
+		threadSafe = false,
+		requiresProject = true,
+		defaultPhase = LifecyclePhase.INTEGRATION_TEST)
 public class RunMojo extends AbstractMojo {
-
+	
 	private static final String[] EMPTY_STRING_ARRAY = new String[] {};
-	private static final String[] DEFAULT_INCLUDE = new String[] {"src/test/varnish/**.vtc"};
+	private static final String[] DEFAULT_INCLUDE = new String[] { "src/test/varnish/**.vtc" };
 	
 	@Component
 	private MavenProject project;
 	
-	@Parameter(defaultValue = "${project.build.directory}/varnishtest")
+	@Parameter(defaultValue = "${project.build.directory}/varnishtest", property = "varnishtest.outputDirectory")
 	private File outputDirectory;
 	
-	@Parameter(defaultValue = "varnishtest")
+	@Parameter(defaultValue = "varnishtest", property = "varnishtest.varnishtestCommand")
 	private String varnishtestCommand;
 	
-	@Parameter(defaultValue = "varnishd")
+	@Parameter(defaultValue = "varnishd", property = "varnishtest.varnishdCommand")
 	private String varnishdCommand;
-    
+	
 	@Parameter
 	private List<String> includes;
 	
-    @Parameter
-    protected List<String> excludes;
+	@Parameter
+	private List<String> excludes;
+	
+	@Parameter
+	private Map<String, String> macros;
 	
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
@@ -46,12 +53,29 @@ public class RunMojo extends AbstractMojo {
 		CommandLine commandLine = new CommandLine(varnishtestCommand);
 		addMacro(commandLine, "varnishd", varnishdCommand);
 		addMacro(commandLine, "project.basedir", getBasedir());
+		addMacros(commandLine);
 		
 		for (String testCase : getTestCases()) {
 			VarnishtestRunner runner = new VarnishtestRunner(commandLine);
 			runner.runTestCase(this, new File(getBasedir(), testCase), 20);
 		}
 		
+	}
+	
+	private void addMacro(CommandLine commandLine, String name, Object value) {
+		commandLine.addArgument("-D" + name + "=" + value);
+	}
+	
+	private void addMacros(CommandLine commandLine) {
+		if (macros == null) {
+			return;
+		}
+		
+		// TODO validate macros map against hard coded keys
+		
+		for (Map.Entry<String, String> macro : macros.entrySet()) {
+			addMacro(commandLine, macro.getKey(), macro.getValue());
+		}
 	}
 	
 	private String[] getTestCases() {
@@ -64,26 +88,22 @@ public class RunMojo extends AbstractMojo {
 		String[] testCases = scanner.getIncludedFiles();
 		return testCases != null ? testCases : EMPTY_STRING_ARRAY;
 	}
-
+	
 	private File getBasedir() {
 		return project.getBasedir();
 	}
-
+	
 	private String[] getExcludes() {
 		if (excludes == null || excludes.isEmpty()) {
 			return EMPTY_STRING_ARRAY;
 		}
 		return excludes.toArray(EMPTY_STRING_ARRAY);
 	}
-
+	
 	private String[] getIncludes() {
 		if (includes == null || includes.isEmpty()) {
 			return DEFAULT_INCLUDE;
 		}
 		return includes.toArray(EMPTY_STRING_ARRAY);
-	}
-	
-	private void addMacro(CommandLine commandLine, String name, Object value) {
-		commandLine.addArgument("-D" + name + "=" + value);
 	}
 }
